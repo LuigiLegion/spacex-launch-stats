624b01c90ca892787a123f86971c12c30b029e5d
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-30 10:02:48 -0500

    feat: set the frontend client as a new ApolloClient, wrapped the App component container div in ApolloProvider with said client as a prop, and plugged the Launches component into it

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-import './App.css';
+import Launches from './components/Launches';
 import logo from '../src/logo.png';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+});
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} alt="SpaceX" className="logo" />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img src={logo} alt="SpaceX" className="logo" />
+
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
