2b7e4f5866696fbabbb8191a21848c710dd06677
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-30 08:46:45 -0500

    feat: plugged logo into App component and created a css class to resize and center it

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+
 import './App.css';
+import logo from '../src/logo.png';
 
 function App() {
-  return <div className="App">SpaceX Launches</div>;
+  return (
+    <div className="App">
+      <img src={logo} alt="SpaceX" className="logo" />
+    </div>
+  );
 }
 
 export default App;
