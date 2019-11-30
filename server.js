662edc876b42dab0eccfd32696bf4f6100baedb3
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-30 09:57:53 -0500

    feat: plugged cors into server as middleware

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+// Imports
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
+
 const schema = require('./schema');
 
+// Initializations
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
 
 app.use(
   '/graphql',
