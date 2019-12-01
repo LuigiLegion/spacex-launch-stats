0cb65bd992db338111a6c0a5bb84950d52262d3d
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-30 23:54:25 -0500

    feat: imported BrowserRouter from react-router-dom alomg with Route, and plugged the main page and the Launch component to their own individual routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 import logo from '../src/logo.png';
 import './App.css';
 
@@ -13,11 +15,14 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img src={logo} alt="SpaceX" className="logo" />
+      <Router>
+        <div className="container">
+          <img src={logo} alt="SpaceX" className="logo" />
 
-        <Launches />
-      </div>
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launches/:flight_number" component={Launches} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
